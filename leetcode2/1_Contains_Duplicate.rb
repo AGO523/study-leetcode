42646916ae5631a337d5e7bf8b8def67f8679799
AGO523 <unpouletfrais0523@gmail.com>
2025-03-21 08:09:23 +0900

    1

--- leetcode2/1_Contains_Duplicate.rb
+++ leetcode2/1_Contains_Duplicate.rb
@@ -1,6 +1,8 @@
+require 'set'
 # @param {Integer[]} nums
 # @return {Boolean}
 def contains_duplicate(nums)
   # nums.uniq.size != nums.size
-  nums.length != nums.uniq.length
+  # nums.length != nums.uniq.length
+  Set.new(nums).length != nums.length
 end
