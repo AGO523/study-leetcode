91c5fdd998e8db2ebb6780a14835b6e555b2802d
AGO523 <unpouletfrais0523@gmail.com>
2025-04-13 09:46:44 +0900

    26

---
+++ leetcode2/26_car_fleet.rb
@@ -0,0 +1,32 @@
+# Input: target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]
+# Output: 3
+
+def car_fleet(target, position, speed)
+  # 位置と速度をペアにして降順で並べる
+  # [[10, 2], [8, 4], [0, 1], [5, 1], [3, 3]]
+  cars = position.zip(speed)
+  # 降順に並べる
+  # [[10, 2], [8, 4], [5, 1], [3, 3], [0, 1]]
+  cars.sort_by { |pos, _| -pos }
+
+  stack = []
+
+  cars.each do |pos, spd|
+    # [10, 2] -> time = (target - 10) / 2 => 1.0
+    # [8, 4] -> time = (target - 8) / 4 => 1.0
+    # [5, 1] -> time = (target - 5) / 1 => 7.0
+    # [3, 3] -> time = (target - 3) / 3 => 3.0
+    # [0, 1] -> time = (target - 0) / 1 => 12.0
+    time = (target - pos).to_f / spd
+
+    # 追いつかない場合だけスタックに追加
+    # 降順のうち、前の車両の到着時間よりも早い場合は fleet に吸収されるので
+    # time > stack[-1]
+    if stack.empty? || time > stack[-1]
+      stack.push(time)
+    end
+    # それ以外（追いつく）は fleet に吸収される
+  end
+
+  stack.size
+end
